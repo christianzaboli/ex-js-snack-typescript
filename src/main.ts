ccbd1ae3eadc4b10b63802e9fb845f1d14b75a1c
osaze <osazezaboli@gmail.com>
2026-02-07 23:52:07 +0100

    first commit basic setup

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Just some TypeScript Snacks!</h1>
+
+    <p class="read-the-docs">
+      Open up the console to check some logs
+    </p>
+  </div>
+`;
