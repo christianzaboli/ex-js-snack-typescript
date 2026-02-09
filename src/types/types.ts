259ad6ebae6c65e9ba1d3d417c6b1f72500ce360
osaze <osazezaboli@gmail.com>
2026-02-09 03:29:49 +0100

    snack 3

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,5 +6,22 @@ export type Dipendete = {
   anniDiServizio: number[];
   readonly emailAziendale: string;
   contratto: "indeterminato" | "determinato" | "freelance";
+};
+
+export type Developer = Dipendete & {
   livelloEsperienza: "Junior" | "Mid" | "Senior";
+  linguaggi?: string[];
+  certificazioni: string[];
+};
+
+export type ProjectManager = Dipendete & {
+  teamSize: number | null;
+  budgetGestito?: number;
+  stakeholderPrincipali: string[];
+};
+export type Team = {
+  nome: string;
+  progettoAttuale: string | null;
+  budget: number;
+  membri: [ProjectManager, Developer, ...Developer[]];
 };
